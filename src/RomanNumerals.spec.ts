fdaccee5e19ff24d650390004ba4f874ee561126
Cristo Daniel Navarro Rodriguez <cristo.daniel.navarro@gmail.com>
2024-06-14 15:26:18 +0100

    red: with 4 returns IV

--- src/RomanNumerals.spec.ts
+++ src/RomanNumerals.spec.ts
@@ -13,4 +13,10 @@ describe("RomanNumerals", () => {
 
     expect(romanNumerals.fromArabic(2)).toBe("II")
   })
+
+  it("with 4 returns IV", () => {
+    const romanNumerals = new RomanNumerals()
+
+    expect(romanNumerals.fromArabic(4)).toBe("IV")
+  })
 })
